c35acb34d259020cba469be3e1ac73a5a0c727ca
Alexander K <a.kazarin@mail.ru>
2022-10-24 14:45:10 +0300

    написаны Модели, зарегистрированы в админ

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import User, Reviews, Titles, Comments, Genres, Categories
 
 # Register your models here.
-
+admin.site.register(User)
+admin.site.register(Reviews)
+admin.site.register(Titles)
+admin.site.register(Comments)
+admin.site.register(Genres)
+admin.site.register(Categories)
